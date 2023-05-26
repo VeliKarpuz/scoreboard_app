099ab69b38612a0a078524c861534ecf008cd3f0
VeliKarpuz <124377172+VeliKarpuz@users.noreply.github.com>
2023-05-26 14:42:38 +0300

    26.05.2023

--- lib/widgets/king/esli_king/esli_king.dart
+++ lib/widgets/king/esli_king/esli_king.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../../const_names.dart';
+
 class EsliKing extends StatelessWidget {
   const EsliKing({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Eşli King"),
+      ),
+      body: const Center(
+        child: Text(ConstNames.tamamlanmadi),
+      ),
+    );
   }
 }
